4f75257f795ff200627e6cbb6d7011ba54b46ff5
marcos5ilva <marcos.silva79@gmail.com>
2017-01-20 22:24:05 -0200

    First commit

---
+++ js/app.js
@@ -0,0 +1,36 @@
+'use strict'
+
+angular.module('app', [])
+
+		.constant("baseURL","http://quotes.stormconsultancy.co.uk/random.json")
+		//.constant("baseURL", "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=movies")
+
+		.controller('RandomQuoteCtrl', ['$scope','$http','baseURL', '$log', function($scope, $http, baseURL, $log) {
+
+			$scope.quote={};
+
+			$http.get(baseURL)
+				.then(function(response) {
+
+				$scope.quote = response;
+				console.log($scope.quote);
+
+			});
+
+			$scope.randomQuote = function() {
+				$http.get(baseURL)
+					.then(function(response) {
+
+						$scope.quote = response;
+						console.log($scope.quote);
+
+				});
+			};
+
+			$scope.tweetIt = function(){
+
+				$scope.share = "https://twitter.com/intent/tweet?text="+ $scope.quote.data.quote +" – "+ $scope.quote.data.author ; 
+				$log.log( "quote obj " + $scope.quote.data);
+			}
+			
+		}])
